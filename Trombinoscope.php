675244f9b6077b71ed7c1841da041b6289d341ef
amelieregnault <77639402+amelieregnault@users.noreply.github.com>
2023-12-13 12:26:47 +0100

    Création de la première vue

--- Trombinoscope.php
+++ Trombinoscope.php
@@ -1,57 +1,14 @@
 <?php
+
+// Récupérer ou calculer ou déclarer les données
 include 'database.php';
-?>
+$page_title = 'Trombinoscope';
+$css = 'style1.css';
 
-<!DOCTYPE html>
+// Construire la vue et l'injecter dans la variable $content
+ob_start();
+include 'app/view/trombi.view.php';
+$content = ob_get_clean();
 
-<html lang="fr">
-
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <title>Trombinoscope</title>
-
-
-
-    <link rel="stylesheet" type="text/css" href="trombi.css">
-</head>
-
-<body>
-
-
-
-    <header>
-        <a href="Trombinoscope.html">
-            <h1>Trombinoscope </h1>
-        </a>
-        <p>Année 2023/2024</p>
-
-    </header>
-    <main>
-
-    <?php foreach ($students as $aStudent): ?>
-
-        <div class="carte">
-            <a href="fiche.html">
-                <?php
-                    if (isset($aStudent['photo'])) {
-                        $photo = 'groupe' . $aStudent['group'] . '/small/' . $aStudent['photo'];
-                    } else {
-                        $photo = 'defaut.png';
-                    }
-                ?>
-
-                <figure class="photo"><img src="./images/<?= $photo ?>" alt="photo de Aimé Mihi">
-                </figure>
-                <div class="infos">
-                    <p class="nom"><?= $aStudent['firstname'] ?> <span><?= $aStudent['lastname'] ?></span></p>
-                    <p class="ddn"><?= $aStudent['birthdate'] ?></p>
-                    <p class="groupe">groupe <span><?= $aStudent['group'] ?></span></p>
-                </div>
-            </a>
-        </div>
-    <?php endforeach ?>
-
-    </main>
-</body>
-
-</html>
+// Génération du code HTML de la page à partir du layout
+include 'app/view/common/layout.php';
