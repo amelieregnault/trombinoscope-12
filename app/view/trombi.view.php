ae1bbe3982204ca652be7f164156b6a1d2dcedc9
amelieregnault <77639402+amelieregnault@users.noreply.github.com>
2024-01-02 19:09:50 +0100

    modification du alt de l'image

--- app/view/trombi.view.php
+++ app/view/trombi.view.php
@@ -10,7 +10,7 @@
             }
         ?>
 
-        <figure class="photo"><img src="public/images/<?= $photo ?>" alt="photo de Aimé Mihi">
+        <figure class="photo"><img src="public/images/<?= $photo ?>" alt="photo de <?= $aStudent['firstname'] ?> <?= $aStudent['lastname'] ?>">
         </figure>
         <div class="infos">
             <p class="nom"><?= $aStudent['firstname'] ?> <span><?= $aStudent['lastname'] ?></span></p>
